e146644844a7ed25e6a9deef82fa9ae89de43ccc
mhwwhu <mhwmahaowen2004@gmail.com>
2024-05-22 16:55:44 +0800

    调整目录

---
+++ SoruxBot.Kernel/Services/PluginService/DataStructure/RadixTreeNode.cs
@@ -0,0 +1,37 @@
+﻿namespace SoruxBot.Kernel.Services.PluginService.DataStructure;
+internal class RadixTreeNode<T>
+{
+    private bool _isValueNode = false;
+    public string Key { get; set; }
+    public T? Value { get; set; } = default;
+    public Dictionary<char, RadixTreeNode<T>> Children { get; set; } = new();
+    public bool IsValueNode { get { return _isValueNode; } }
+    public RadixTreeNode(string key)
+    {
+        Key = key;
+    }
+    public RadixTreeNode(string key, T value)
+    {
+        Key = key;
+        Value = value;
+        _isValueNode = true;
+    }
+    public RadixTreeNode(string key, Dictionary<char, RadixTreeNode<T>> children, bool isValueNode, T? value)
+    {
+        Key = key;
+        Children = children;
+        _isValueNode = isValueNode;
+        Value = value;
+    }
+
+    public void DeleteValue()
+    {
+        Value = default;
+        _isValueNode = false;
+    }
+    public void SetValue(T value)
+    {
+        Value = value;
+        _isValueNode = true;
+    }
+}
---
+++ SoruxBot.Kernel/Services/StorageService/PluginsDataStorage.cs
@@ -0,0 +1,13 @@
+﻿using SoruxBot.Kernel.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoruxBot.Kernel.Services.StorageService
+{
+	public class PluginsDataStorage : IPluginsDataStorage
+	{
+		public PluginsDataStorage(IConfiguration config)
+		{
+
+		}
+	}
+}
