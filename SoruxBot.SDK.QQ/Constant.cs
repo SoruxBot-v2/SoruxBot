8a95b8ce843f48a6be9a10f3056338c5daf74e85
EpicMo <1982742309@qq.com>
2024-05-28 21:49:37 +0800

    feat:add constant

---
+++ SoruxBot.SDK.QQ/Constant.cs
@@ -0,0 +1,9 @@
+namespace SoruxBot.SDK.QQ;
+
+public static class Constant
+{
+    /// <summary>
+    /// 表示 QQ 平台约束的 PlatformType
+    /// </summary>
+    public const string QqPlatformType = "QQ";
+}
