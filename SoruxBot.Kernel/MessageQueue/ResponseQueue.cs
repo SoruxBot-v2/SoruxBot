9110032a5da218c417919186e25391b25699ea84
forDecember <1921567337@qq.com>
2024-05-28 21:44:28 +0800

    update: 重写ChannelPool

--- SoruxBot.Kernel/MessageQueue/ResponseQueue.cs
+++ SoruxBot.Kernel/MessageQueue/ResponseQueue.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Threading.Channels;
 using SoruxBot.Kernel.Interface;
 using SoruxBot.SDK.Model.Message;
 using SoruxBot.SDK.Plugins.Service;
@@ -21,11 +22,17 @@ public class ResponseQueueImpl(
         {
             // 这里是发送给指定用户实体的id
             var bindId = context.TargetPlatform + context.TriggerPlatformId + context.TriggerId;
-            _bindIds[bindId] = true;
 
-
-            await msgChannelPool.CreateBindChannel(bindId).Writer.WriteAsync(context);
-            var res = await syncChannelPool.CreateBindChannel(bindId).Reader.ReadAsync();
+            await msgChannelPool.RentChannel(bindId).Writer.WriteAsync(context);
+            Console.WriteLine("发完了上一个消息");
+            if (!_bindIds[bindId])
+            {
+                _bindIds[bindId] = true;
+            }else
+            {
+                Console.WriteLine("Error");
+            }
+            var res = await syncChannelPool.RentChannel(bindId).Reader.ReadAsync();
 
             // 归还同步channel
             syncChannelPool.ReturnChannel(bindId);
@@ -37,23 +44,29 @@ public class ResponseQueueImpl(
 
     public IResponsePromise SetNextResponse(MessageContext context)
     {
-
-		var promise = new ResponsePromise();
+        var promise = new ResponsePromise();
 
         // 这里是发送给指定用户实体的id
         var bindId = context.TargetPlatform + context.TriggerPlatformId + context.TriggerId;
-        _bindIds[bindId] = true;
 
         new Task<Task>(async () =>
         {
-	        Console.WriteLine("要发了~~~");
-			if(!msgChannelPool.CreateBindChannel(bindId).Writer.TryWrite(context))
-			{
-				Console.WriteLine("Error");
-			}
+            Console.WriteLine("要发了~~~");
+            await msgChannelPool.RentChannel(bindId).Writer.WriteAsync(context);
 
-			
-            var res = await syncChannelPool.CreateBindChannel(bindId).Reader.ReadAsync();
+            Console.WriteLine("发完了上一个消息");
+            if (!_bindIds.TryGetValue(bindId, out var value))
+            {
+                _bindIds.TryAdd(bindId, true);
+
+            }
+            else
+            {
+                Console.WriteLine("Error");
+
+            }
+
+            var res = await syncChannelPool.RentChannel(bindId).Reader.ReadAsync();
 
             promise.Callbacks.ForEach(callback => callback(res));
             // 归还同步channel
@@ -66,63 +79,35 @@ public class ResponseQueueImpl(
 
     public bool TryGetNextResponse(Func<MessageContext, MessageResult> func)
     {
+        MessageContext? context = null;
+        
+        foreach (var t in _bindIds.Keys)
+        {
+            if (!msgChannelPool.TryGetBindChannel(t, out var channel)) continue;
+            if (!channel!.Reader.TryRead(out context)) continue;
+            // 归还消息channel
+            Console.WriteLine(context.TriggerId);
 
+            msgChannelPool.ReturnChannel(t);
+            _bindIds.Remove(t, out _);
+            break;
+        }
 
-	    if (msgChannelPool.TryGetBindChannel("a", out var t))
-	    {
-		    if (t.Reader.TryRead(out var ctx))
-		    {
-			    func(ctx);
-			    Console.WriteLine(ctx);
-		    }
+        // 如果没有有消息的channel，或者没拿到syncChannel，或者写入失败，返回false
+        if (context == null)
+        {
+            return false;
+        }
 
-		    return true;
-	    }
+        Console.WriteLine("进来了");
 
-	    return false;
-		    
-			MessageContext? context = null;
+        if (!syncChannelPool.TryGetBindChannel(
+                context.TargetPlatform + context.TriggerPlatformId + context.TriggerId,
+                out var syncChannel))
+        {
+            Console.WriteLine("出错了");
+        }
 
-			foreach (var (key, _) in _bindIds)
-			{
-				Console.WriteLine("TryGetBindChannel");
-
-				if (!msgChannelPool.TryGetBindChannel(key, out var channel))
-				{
-					continue;
-				}
-
-				if (!channel!.Reader.TryRead(out context))
-				{
-					Console.WriteLine("aaa");
-				}
-				else
-				{
-					Console.WriteLine(context);
-				}
-				// 归还消息channel
-				msgChannelPool.ReturnChannel(key);
-				_bindIds.Remove(key, out _);
-				break;
-			}
-
-
-			// 如果没有有消息的channel，或者没拿到syncChannel，或者写入失败，返回false
-			if (context == null)
-			{
-				return false;
-			}
-
-			Console.WriteLine("进来了");
-
-			if (!syncChannelPool.TryGetBindChannel(
-					   context.TargetPlatform + context.TriggerPlatformId + context.TriggerId,
-					   out var syncChannel))
-			{
-				Console.WriteLine("出错了");
-			}
-
-			return syncChannel!.Writer.TryWrite(func(context));
-		
+        return syncChannel!.Writer.TryWrite(func(context));
     }
 }
